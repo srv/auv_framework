679cd300ba4e2fcac170c4359dd6d69a7de9e619
Stephan Wirth <stephan.wirth@uib.es>
2012-10-10 15:20:00 +0200

    refactored altitude_control

--- src/altitude_controller.py
+++ src/altitude_controller.py
@@ -4,70 +4,48 @@ PACKAGE = 'auv_control'
 
 import roslib; roslib.load_manifest(PACKAGE)
 import rospy
-import sys
-import argparse
 import dynamic_reconfigure.server
 from altitude_controller.cfg import AltitudeControllerConfig
-from pid import PID
-from std_msgs.msg import Float32
-from srv_msgs.msg import Depth
-from geometry_msgs.msg import WrenchStamped
+from pid import Pid
+from sensor_msgs.msg import Range
+from geometry_msgs.msg import Wrench
 
 class AltitudeControllerNode():
     """
-    Node for calculating the thrust needed in the motors
-    in order to keep the altitude to the seabed constant
-    Publishes wrench topic to the motors while subscribes
-    to joystick motor levels and altitude or depth topics.
-    """
+    Node for calculating the wrench needed in order to keep the altitude to the 
+    seabed constant. As only one DOF -namely z- is controlled, the
+    other values have to be given by some other input, e.g. via joystick.
 
-    def __init__(self, input_subscriber, frequency):
-        kp = 0
-        ki = 0
-        kd = 0
-        derivator = 0
-        integrator = 0
-        antiwindup_max = 100;
-        antiwindup_min = -100;
-        self.user_wrench_levels = WrenchStamped()
+    This node subscribes to 'altitude', expecting a sensor_msgs/Range message
+    and to 'wrench_input' which should be of type geometry_msgs/Wrench.
+    The node also receives std_msgs/Float32 messages on the topic 
+    'altitude_request' to define the required altitude (setpoint).
+    The node then calculates the force in z needed to control the AUV to
+    have the desired altitude, replaces this value within wrench_input and
+    publishes the result to 'wrench_output'.
+
+    Internally a simple PID is used for control, its variables can be modified
+    using dynamic_reconfigure.
+    """
+    def __init__(self, frequency, min_force, max_force, k_p, k_i, k_d):
+        self.wrench_input = Wrench()
         self.started = False;
-        self.PID = PID(kp,ki,kd,derivator,integrator,antiwindup_max,antiwindup_min)
+        self.pid = pid(k_p, k_i, k_d, min_force, max_force)
         self.server = dynamic_reconfigure.server.Server(AltitudeControllerConfig, self.reconfigure)
         
-        self.pub = rospy.Publisher('motor_wrench_levels', WrenchStamped)
+        self.pub = rospy.Publisher('wrench_output', Wrench)
         
-        rospy.Subscriber('joy_wrench_levels', WrenchStamped, self.joyCallback)
-        rospy.Subscriber('controller_setpoint', Float32, self.setpointCallback)
+        rospy.Subscriber('wrench_input', WrenchStamped, self.wrenchInputCallback)
+        rospy.Subscriber('altitude_request', Float32, self.setpointCallback)
         
-        period = rospy.rostime.Duration.from_sec(1/frequency)
+        period = rospy.rostime.Duration.from_sec(1.0/frequency)
         self.timer = rospy.Timer(period, self.updateOutput)
         
-        assert input_subscriber == 'depth' or input_subscriber == 'altitude', "input can only take as value altitude or depth: %r" % input
-        if input_subscriber == 'depth':
-            rospy.Subscriber('depth', Depth, self.depthCallback)
-        elif input_subscriber == 'altitude':
-            rospy.Subscriber('altitude', Float32, self.altitudeCallback)
-        
-        
+        rospy.Subscriber('altitude', Float32, self.altitudeCallback)
         
     def reconfigure(self, config, level):
         rospy.loginfo("Reconfigure request...")
-        
-        self.PID.setKp(config['Kp'])
-        config['Kp'] = self.PID.Kp
-        
-        self.PID.setKi(config['Ki'])
-        config['Ki'] = self.PID.Ki
-        
-        self.PID.setKd(config['Kd'])
-        config['Kd'] = self.PID.Kd 
-        
-        self.PID.setIntegrator(config['Integrator'])
-        config['Integrator'] = self.PID.getIntegrator()
-        
-        self.PID.setDerivator(config['Derivator'])
-        config['Derivator'] = self.PID.getDerivator()
-      
+        self.pid.setConstants(config['Kp'], config['Ki'], config['Kd'])
         rospy.loginfo("Reconfigured!")
         return config # Returns the updated configuration.
     
@@ -76,50 +54,36 @@ class AltitudeControllerNode():
         Change the setPoint of the controller
         """
         rospy.loginfo('Changed setpoint to: %s', setpoint.data)
-        self.PID.setPoint(setpoint.data)
+        self.pid.setSetpoint(setpoint.data)
 
     def altitudeCallback(self, altitude):
-        """
-        Update the output with the new feedback
-        self.error = self.set_point - current_value
-        """
-        self.currentValue = altitude.data
+        self.current_altitude = altitude.data
         if not self.started:
             self.started = True
         
-    def depthCallback(self, depth):
-        """
-        Update the output with the new feedback
-        self.error = self.set_point - current_value
-        """
-        self.currentValue= depth.data
-        if not self.started:
-            self.started = True
-        
-    def joyCallback(self, joy_wrench_levels):
-        self.user_wrench_levels = joy_wrench_levels;
+    def wrenchInputCallback(self, wrench):
+        self.wrench_input = wrench
         
     def updateOutput(self, event):
         if self.started:
-            self.output = self.PID.update(self.currentValue)
-            motor_wrench_levels = self.user_wrench_levels
-            if self.output > 1.0:
-                self.output = 1.0
-            elif self.output < -1.0:
-                self.output = -1.0
-            motor_wrench_levels.wrench.force.z = self.output
-            self.pub.publish(motor_wrench_levels)
+            wrench_output = self.wrench_input
+            dt = (event.current_real - event.last_real).toSec()
+            wrench_output.force.z = self.pid.update(self.current_altitude, dt)
+            self.pub.publish(wrench_output)
         else:
             rospy.loginfo('Waiting topics to be published...')
 
 if __name__ == "__main__":
     rospy.init_node('altitude_controller')
-    parser = argparse.ArgumentParser(description='PID controller for the altitude of a submarine')
-    parser.add_argument('-i','--input', default='altitude', help='input to controller (altitude or depth)')
-    parser.add_argument('-f','--frequency',type=float, default=5.0, help='controller frequency in hertz')
-    args = parser.parse_args(rospy.myargv()[1:])    
     try:
-        node = AltitudeControllerNode(args.input,args.frequency)
+        frequency = rospy.get_param("~frequency", 10.0)
+        min_force = rospy.get_param("~min_force", -100.0);
+        max_force = rospy.get_param("~max_force",  100.0);
+        k_p = rospy.get_param("~Kp",  0.0);
+        k_i = rospy.get_param("~Ki",  0.0);
+        k_d = rospy.get_param("~Kd",  0.0);
+        node = AltitudeControllerNode(
+                frequency, min_force, max_force, k_p, k_i, k_d)
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
