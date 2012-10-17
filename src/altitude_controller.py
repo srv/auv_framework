c9a7650b97d680afca949e8f5668897adf80ff49
Stephan Wirth <stephan.wirth@uib.es>
2012-10-17 14:46:25 +0200

    new enable service, several sanity checks

--- src/altitude_controller.py
+++ src/altitude_controller.py
@@ -5,6 +5,8 @@ PACKAGE = 'auv_control'
 import roslib; roslib.load_manifest(PACKAGE)
 import rospy
 import dynamic_reconfigure.server
+import copy
+import auv_control.srv
 from auv_control.cfg import AltitudeControllerConfig
 from pid import Pid
 from sensor_msgs.msg import Range
@@ -27,24 +29,60 @@ class AltitudeControllerNode():
 
     Internally a simple PID is used for control, its variables can be modified
     using dynamic_reconfigure.
+
+    A service is provided to enable and disable the altitude control, by default
+    it is disabled. Enabling sets the setpoint to the current altitude.
     """
     def __init__(self, frequency, k_p, k_i, k_d):
+        self.FEEDBACK_TIMEOUT = 0.5 # half a second
         self.wrench_input = WrenchStamped()
-        self.started = False;
+        self.setpoint_valid = False
+        self.feedback_received = False
+        self.enabled = False
+        self.last_feedback = Range()
+        self.enable_server = rospy.Service('~enable', auv_control.srv.EnableControl, self.enable)
         self.pid = Pid(k_p, k_i, k_d)
         self.server = dynamic_reconfigure.server.Server(AltitudeControllerConfig, self.reconfigure)
         
-        self.pub = rospy.Publisher('wrench_output', WrenchStamped)
+        self.pub = rospy.Publisher('~wrench_output', WrenchStamped)
         
-        rospy.Subscriber('wrench_input', WrenchStamped, self.wrenchInputCallback)
-        rospy.Subscriber('altitude_request', Float32, self.setpointCallback)
+        rospy.Subscriber('~wrench_input', WrenchStamped, self.wrenchInputCallback)
+        rospy.Subscriber('~altitude_request', Float32, self.setpointCallback)
         
         period = rospy.rostime.Duration.from_sec(1.0/frequency)
         self.timer = rospy.Timer(period, self.updateOutput)
         
         rospy.Subscriber('altitude', Range, self.altitudeCallback)
-        
+        rospy.loginfo('Listening for altitude feedback to be published on '
+                      '%s...', rospy.resolve_name('altitude'))
+        rospy.loginfo('Waiting for setpoint to be published on '
+                      '%s...', rospy.resolve_name('~altitude_request'))
+
+    def enable(self, request):
+        """
+        Handles ROS service requests for enabling/disabling control.
+        Returns current enabled status and setpoint.
+        """
+        response = auv_control.srv.EnableControlResponse()
+        if request.enable:
+            if self.isFeedbackValid():
+                self.enabled = True
+                self.pid.setSetpoint(self.last_feedback.range)
+                self.setpoint_valid = True
+                response.enabled = True
+            else:
+                rospy.logerr("Cannot enable altitude control without valid feedback!")
+                response.enabled = False
+        else:
+            self.enabled = False
+            response.enabled = False
+        response.current_setpoint = self.pid.getSetpoint()
+        return response
+
     def reconfigure(self, config, level):
+        """
+        Handles dynamic reconfigure requests.
+        """
         rospy.loginfo("Reconfigure request...")
         self.pid.k_p = config['Kp']
         self.pid.k_i = config['Ki']
@@ -55,28 +93,40 @@ class AltitudeControllerNode():
     
     def setpointCallback(self,setpoint):
         """
-        Change the setPoint of the controller
+        Change the setpoint of the controller.
         """
+        if not self.setpoint_valid:
+            rospy.loginfo("First setpoint received.")
+            self.setpoint_valid = True
         rospy.loginfo('Changed setpoint to: %s', setpoint.data)
         self.pid.setSetpoint(setpoint.data)
 
     def altitudeCallback(self, range_msg):
-        self.current_altitude = range_msg.range
-        if not self.started:
-            self.started = True
+        self.last_feedback = range_msg
         
     def wrenchInputCallback(self, wrench):
         self.wrench_input = wrench
+
+    def isFeedbackValid(self):
+        feedback_in_time = (rospy.Time.now() - 
+            self.last_feedback.header.stamp).to_sec() < self.FEEDBACK_TIMEOUT
+        return feedback_in_time and (self.last_feedback.range > 0)
         
     def updateOutput(self, event):
-        if self.started:
-            wrench_output = self.wrench_input
-            dt = (event.current_real - event.last_real).toSec()
-            wrench_output.wrench.force.z = self.pid.update(self.current_altitude, dt)
-            self.pub.publish(wrench_output)
-        else:
-            rospy.loginfo('Waiting for altitude feedback to be published on '
-                          '%s...', rospy.resolve_name('altitude'))
+       if self.setpoint_valid and self.enabled:
+           wrench_output = copy.deepcopy(self.wrench_input)
+           if self.isFeedbackValid():
+               dt = (event.current_real - event.last_real).to_sec()
+               wrench_output.wrench.force.z = -self.pid.update(self.last_feedback.range, dt)
+               if wrench_output.wrench.force.z > 1.0:
+                   wrench_output.wrench.force.z = 1.0
+               if wrench_output.wrench.force.z < -1.0:
+                   wrench_output.wrench.force.z = -1.0
+           else:
+               rospy.logwarn("Altitude feedback is invalid, setting force in z to zero.")
+               wrench_output.wrench.force.z = 0.0
+           wrench_output.header.stamp = rospy.Time.now()
+           self.pub.publish(wrench_output)
 
 if __name__ == "__main__":
     rospy.init_node('altitude_controller')
@@ -94,8 +144,8 @@ if __name__ == "__main__":
                       '  wrench_input: %s \n'
                       ' wrench_output: %s \n', frequency, k_p, k_i, k_d,
                       rospy.resolve_name('altitude'), 
-                      rospy.resolve_name('wrench_input'),
-                      rospy.resolve_name('wrench_output'))
+                      rospy.resolve_name('~wrench_input'),
+                      rospy.resolve_name('~wrench_output'))
 
         node = AltitudeControllerNode(
                 frequency, k_p, k_i, k_d)
